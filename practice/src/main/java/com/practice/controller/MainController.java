1936a07f01c3d841df067ae39670821fd0ca14ce
junye <junye@DESKTOP-EEPHA35.kornet>
2024-12-31 23:29:31 +0900

    data binding

--- practice/src/main/java/com/practice/controller/MainController.java
+++ practice/src/main/java/com/practice/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.practice.controller;
 
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.practice.dto.BookDto;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
@@ -20,16 +27,22 @@ public class MainController {
 
 	// Form Create 요청
 	@PostMapping("/bookCForm")
-	public String bookCForm() {
+	public String bookCForm(@ModelAttribute BookDto bookDto,@RequestHeader HttpHeaders headers, HttpServletRequest cookie,@CookieValue("sessionId") String sessionId) {
 		System.out.println("폼 요청확인");
+		System.out.printf("book : %s\n",bookDto.toString());
+		System.out.println("headers : "+headers);
+		System.out.println("headers2 : "+sessionId);
+		System.out.println("cookie : " + cookie.getCookies());
+		
 		return "home";
 	}
 	//통합
 	@ResponseBody
 	@PostMapping("/bookC/{type}")
-	public String bookCType(@PathVariable("type") String type,@RequestBody BookDto bookDto) {
+	public String bookCType(@PathVariable("type") String type,@RequestBody BookDto bookDto,@RequestHeader("Content-Type") String header1) {
 		System.out.printf("%s 요청확인\n",type);
 		System.out.printf("book : %s\n",bookDto.toString());
+		System.out.println("header1:"+header1);
 		return "";
 	}
 //20241230
--- practice/src/main/resources/static/js/script.js
+++ practice/src/main/resources/static/js/script.js
@@ -1,43 +1,45 @@
-function sendForm(requestType){
+function sendForm(requestType) {
 	const url = "/bookC/"
 	const title = document.querySelector("#title").value
 	const author = document.querySelector("#author").value
 	const publishedDate = document.querySelector("#publishedDate").value
 	const price = document.querySelector("#price").value
-	console.log("requestType: ",requestType)
+	console.log("requestType: ", requestType)
 	const data = {
-		title : title,
-		author : author,
-		publishedDate : publishedDate,
-		price : price
+		title: title,
+		author: author,
+		publishedDate: publishedDate,
+		price: price
 	}
 	console.log(data)
-	if(requestType == "jquery"){
+	if (requestType == "jquery") {
 		$.ajax({
-			url : url + requestType,
-			type : "post",
-			contentType : "application/json",
-			data : JSON.stringify(data),
-			success : data=>console.log("jquery요청 성공")
+			url: url + requestType,
+			type: "post",
+			contentType: "application/json",
+			data: JSON.stringify(data),
+			success: data => console.log("jquery요청 성공")
 		})
-	}else if(requestType == "fetch"){
-		fetch(url + requestType,{
-			method : "post",
-			headers : {},
-			body : {}	
-			}
-			
+	} else if (requestType == "fetch") {
+		fetch(url + requestType, {
+			method: "post",
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
+		}
+
 		)
-		.then(data => console.log("fetch요청 성공"))
-		
-	}else if(requestType == "axios"){
+			.then((data) => {
+				console.log("fetch요청 성공");
+			})
+
+	} else if (requestType == "axios") {
 		axios({
-			url : url + requestType,
-			method : "post",
-			headers : {},
-			data : {}
+			url: url + requestType,
+			method: "post",
+			headers: {},
+			data: data
 		})
-		.then(data=>console.log("axios요청 성공"))
+			.then(data => console.log("axios요청 성공"))
 	}
 }
 
